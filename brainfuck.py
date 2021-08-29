f3f2b5cce4998e49e9ca2614ac55589648fc99dd
Tanya Nevskaya <unparalloser@gmail.com>
2021-08-29 21:14:01 +0300

    Refactor the code

--- brainfuck.py
+++ brainfuck.py
@@ -1,40 +1,45 @@
-import sys
-import getch
+#!/usr/bin/env python
 
-cells = [0] * 10000
+from sys import argv
+from getch import getch
 
-f = open(sys.argv[1], 'r')
-code = f.read()
+def brainfuck(code):
+    cells = [0] * 30000
 
-pointer = 0
-code_i = 0
-brackets = {}
+    cells_i = 0
+    code_i = 0
+    loops = {}
 
-matching_queue = []
-for i, c in enumerate(code):
-    if c == '[':
-        matching_queue.append(i)
-    elif c == ']':
-        opening_bracket = matching_queue.pop()
-        brackets[i] = opening_bracket
-        brackets[opening_bracket] = i
+    loop_matches = []
+    for i, c in enumerate(code):
+        if c == '[':
+            loop_matches.append(i)
+        elif c == ']':
+            loop_start_i = loop_matches.pop()
+            loops[i] = loop_start_i
+            loops[loop_start_i] = i
 
-while code_i < len(code):
-    c = code[code_i]
-    if c == '>':
-        pointer += 1
-    elif c == '<':
-        pointer -= 1
-    elif c == '+':
-        cells[pointer] += 1
-    elif c == '-':
-        cells[pointer] -= 1
-    elif c == '.':
-        print(chr(cells[pointer]), end='')
-    elif c == ',':
-        cells[pointer] = ord(getch.getch())
-    elif c == '[' and cells[pointer] == 0:
-        code_i = brackets[code_i]
-    elif c == ']' and cells[pointer] != 0:
-        code_i = brackets[code_i]
-    code_i += 1
+    while code_i < len(code):
+        c = code[code_i]
+        if c == '>':
+            cells_i += 1
+        elif c == '<':
+            cells_i -= 1
+        elif c == '+':
+            cells[cells_i] += 1
+        elif c == '-':
+            cells[cells_i] -= 1
+        elif c == '.':
+            print(chr(cells[cells_i]), end='')
+        elif c == ',':
+            cells[cells_i] = ord(getch())
+        elif c == '[' and cells[cells_i] == 0:
+            code_i = loops[code_i]
+        elif c == ']' and cells[cells_i] != 0:
+            code_i = loops[code_i]
+        code_i += 1
+
+if __name__ == '__main__':
+    with open(argv[1], 'r') as f:
+        code = f.read()
+        brainfuck(code)
